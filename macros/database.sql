be2d5bf0cd652f7739c12ac4f9bd909a4f2204cc
Erlend Barstad <erlend.barstad@sparebank1.no>
2025-03-18 08:48:51 +0000

    feat: macros for drop and create database

---
+++ macros/database.sql
@@ -0,0 +1,39 @@
+{% macro default__create_database(database_name) %}
+  {% set project_name = target.name.split("__")[0] %}
+  {% set clean_user_name = target.user.split("@")[0] | replace('.', '') | replace('-', '') %}
+  {% set upper_database_name = database_name | upper %}
+  {% set full_database_name = project_name ~ '__' ~ clean_user_name ~ '__' ~ upper_database_name %}
+    
+  {%- call statement('create_database') -%}
+    CALL DATAPLATTFORM.{{ project_name }}.CREATE_OR_REPLACE_DATABASE(
+      '{{ full_database_name }}',
+      ARRAY_CONSTRUCT('{{ project_name }}_NON_PROD_ROLE')
+    )
+  {% endcall %}
+  {{ log("Database " ~ full_database_name ~ " has been created", info=True) }}
+{% endmacro %}
+
+{% macro create_database(database_name) %}
+    {{ return(adapter.dispatch('create_database')(database_name)) }}
+{% endmacro %}
+
+
+{% macro default__drop_database(database_name) %}
+  {% set project_name = target.name.split("__")[0] %}
+  {% set clean_user_name = target.user.split("@")[0] | replace('.', '') | replace('-', '') %}
+  {% set upper_database_name = database_name | upper %}
+  {% set full_database_name = project_name ~ '__' ~ clean_user_name ~ '__' ~ upper_database_name %}
+    
+  {%- call statement('drop_database') -%}
+    CALL DATAPLATTFORM.{{ project_name }}.DROP_DATABASE(
+      '{{ full_database_name }}'
+    )
+  {% endcall %}
+  {{ log("Database " ~ full_database_name ~ " has been dropped", info=True) }}
+{% endmacro %}
+
+{% macro drop_database(database_name) %}
+    {{ return(adapter.dispatch('drop_database')(database_name)) }}
+{% endmacro %}
+
+
