c89d7b8decdc4444fecc08908eb4a8ecee95140d
Erlend Barstad <erlend.barstad@sparebank1.no>
2025-03-18 14:54:26 +0000

    add saparate macro for replace db

--- macros/database.sql
+++ macros/database.sql
@@ -1,3 +1,23 @@
+{% macro default__create_or_replace_database(database_name) %}
+  {% set project_name = target.name.split("__")[0] %}
+  {% set clean_user_name = target.user.split("@")[0] | replace('.', '') | replace('-', '') %}
+  {% set upper_database_name = database_name | upper %}
+  {% set full_database_name = project_name ~ '__' ~ clean_user_name ~ '__' ~ upper_database_name %}
+    
+  {%- call statement('create_or_replace_database') -%}
+    CALL DATAPLATTFORM.{{ project_name }}.CREATE_OR_REPLACE_DATABASE(
+      '{{ full_database_name }}',
+      ARRAY_CONSTRUCT('{{ project_name }}_NON_PROD_ROLE')
+    )
+  {% endcall %}
+  {{ log("Database " ~ full_database_name ~ " has been created", info=True) }}
+{% endmacro %}
+
+{% macro create_or_replace_database(database_name) %}
+    {{ return(adapter.dispatch('create_or_replace_database')(database_name)) }}
+{% endmacro %}
+
+
 {% macro default__create_database(database_name) %}
   {% set project_name = target.name.split("__")[0] %}
   {% set clean_user_name = target.user.split("@")[0] | replace('.', '') | replace('-', '') %}
@@ -5,12 +25,12 @@
   {% set full_database_name = project_name ~ '__' ~ clean_user_name ~ '__' ~ upper_database_name %}
     
   {%- call statement('create_database') -%}
-    CALL DATAPLATTFORM.{{ project_name }}.CREATE_OR_REPLACE_DATABASE(
+    CALL DATAPLATTFORM.{{ project_name }}.CREATE_DATABASE(
       '{{ full_database_name }}',
       ARRAY_CONSTRUCT('{{ project_name }}_NON_PROD_ROLE')
     )
   {% endcall %}
-  {{ log("Database " ~ full_database_name ~ " has been created", info=True) }}
+  {{ log("Database " ~ full_database_name ~ " has been created or already exsists", info=True) }}
 {% endmacro %}
 
 {% macro create_database(database_name) %}
