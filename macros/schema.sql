71f690a642314ffaff029bc40db5bec9edb80750
Imre Kerr <imre.kerr@sparebank1.no>
2025-12-17 11:15:19 +0000

    fix: add current_role() as parameter to procedure calls

--- macros/schema.sql
+++ macros/schema.sql
@@ -1,13 +1,13 @@
 {% macro default__create_schema(relation) -%}
   {%- call statement('create_schema') -%}
     {% set project_name = target.name.split("__")[0] %}
-    CALL dataplattform.{{ project_name }}.create_schema('{{ relation.database }}', '{{ relation.schema }}')
+    CALL dataplattform.{{ project_name }}.create_schema(current_role(), '{{ relation.database }}', '{{ relation.schema }}')
   {% endcall %}
 {% endmacro %}
 
 {% macro default__drop_schema(relation) -%}
   {%- call statement('drop_schema') -%}
     {% set project_name = target.name.split("__")[0] %}
-    CALL dataplattform.{{ project_name }}.drop_schema('{{ relation.database }}', '{{ relation.schema }}')
+    CALL dataplattform.{{ project_name }}.drop_schema(current_role(), '{{ relation.database }}', '{{ relation.schema }}')
   {% endcall %}
 {% endmacro %}
